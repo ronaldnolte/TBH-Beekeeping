ff3a4fde88f89a2e1679645651d958f13e164529
Ronald Nolte <ron.nolte@gmail.com>
2025-12-19 12:28:16 -0700

    fix: Move pnpm overrides to root package.json to correctly force React 18

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
     reactStrictMode: false,
-    webpack: (config) => {
-        const path = require('path');
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            'react': path.resolve(__dirname, 'node_modules/react'),
-            'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-        };
-        return config;
-    },
 }
