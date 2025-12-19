cd8b2edfb39a5375608a9cff205f15c39d4bdf7d
Ronald Nolte <ron.nolte@gmail.com>
2025-12-19 12:25:04 -0700

    fix: Webpack alias to enforce local React resolution

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     reactStrictMode: false,
+    webpack: (config) => {
+        const path = require('path');
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react': path.resolve(__dirname, 'node_modules/react'),
+            'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+        };
+        return config;
+    },
 }
